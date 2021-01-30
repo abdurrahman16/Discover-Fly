7c37088a85d9eb019c627a2f34a0feaa5cc84f32
Mohammad Abdur Rahman <rahman.just16@gmail.com>
2021-01-30 15:45:35 +0600

    get your ticket now!

--- index.js
+++ index.js
@@ -1,91 +1,110 @@
 document.getElementById('firstClass-Increase').addEventListener('click', function(){
-    const firstClassInput = document.getElementById('firstClass-count');
-    const fCount = parseInt(firstClassInput.value);
-     const fClassNewCount = fCount + 1;
-     firstClassInput.value = fClassNewCount;
-    var fTotal = fClassNewCount * 150;
+    // const firstClassInput = document.getElementById('firstClass-count');
+    // const fCount = parseInt(firstClassInput.value);
+    //  const fClassNewCount = fCount + 1;
+    //  firstClassInput.value = fClassNewCount;
+    // var fTotal = fClassNewCount * 150;
 
-     console.log(fTotal);
-     
+    firstClassBill(true);
+    
     });
 
 
     document.getElementById('firstClass-decrease').addEventListener('click', function(){
-        const firstClassInput = document.getElementById('firstClass-count');
-        const fCount = parseInt(firstClassInput.value);
-         const fClassNewCount = fCount - 1;
-         firstClassInput.value = fClassNewCount;
-         var fTotal = fClassNewCount * 150;
-         
-        
+        // const firstClassInput = document.getElementById('firstClass-count');
+        // const fCount = parseInt(firstClassInput.value);
+        //  const fClassNewCount = fCount - 1;
+        //  firstClassInput.value = fClassNewCount;
+        //  var fTotal = fClassNewCount * 150;     
+        firstClassBill(false);  
          
         });
 
 
-
-/////////////////////////////////
-
+// eco class level
 
 document.getElementById('economyClass-Increase').addEventListener('click', function(){
+    // const ecoClassInput = document.getElementById('economyClass-count');
+    //  const ecoCount = parseInt(ecoClassInput.value);
+    // const ecoClassNewCount = ecoCount + 1;
+    // ecoClassInput.value = ecoClassNewCount;
+    //  var ecoTotal = ecoClassNewCount * 100;  
+
+    ecoClassBill (true);
+
+});
+
+
+document.getElementById('economyClass-decrease').addEventListener('click', function(){
+
+    ecoClassBill (false);
+
+});
+
+// bill for first class
+
+       function ecoClassBill (IsIncrease){
+        const ecoClassInput = document.getElementById('economyClass-count');
+     const ecoCount = parseInt(ecoClassInput.value);
+
+        let ecoClassNewCount = ecoCount;
+        if (IsIncrease == true){
+            ecoClassNewCount = ecoCount + 1;
+        }
+        if (IsIncrease == false && ecoClassNewCount>0){
+            ecoClassNewCount = ecoCount - 1;
+        }
+      
+        ecoClassInput.value = ecoClassNewCount;
+     var ecoTotal = ecoClassNewCount * 100; 
+     SubBill ();
+
+       }
+
+       // economy class function
+
+       function firstClassBill (IsIncrease){
+        const firstClassInput = document.getElementById('firstClass-count');
+        const fCount = parseInt(firstClassInput.value);
+
+        let fClassNewCount = fCount;
+        if (IsIncrease == true){
+            fClassNewCount = fCount + 1;
+        }
+        if (IsIncrease == false && fClassNewCount>0){
+            fClassNewCount = fCount - 1;
+        }
+      
+        firstClassInput.value = fClassNewCount;
+        var fTotal = fClassNewCount * 150;
+        SubBill ();
+
+       }
+       
+/// sub total
+       
+       
+        function SubBill (){
+
             const ecoClassInput = document.getElementById('economyClass-count');
             const ecoCount = parseInt(ecoClassInput.value);
-             const ecoClassNewCount = ecoCount + 1;
-             ecoClassInput.value = ecoClassNewCount;
-             var ecoTotal = ecoClassNewCount * 100;
-            
-             
-            });
-        
-        
-            document.getElementById('economyClass-decrease').addEventListener('click', function(){
-                const ecoClassInput = document.getElementById('economyClass-count');
-                const ecoCount = parseInt(ecoClassInput.value);
-                 const ecoClassNewCount = ecoCount - 1;
-                 ecoClassInput.value = ecoClassNewCount;
-                 var ecoTotal = ecoClassNewCount * 100;
-                
-                // // autoMation(true);
-              
-                 
-                });
+
+            const firstClassInput = document.getElementById('firstClass-count');
+            const fCount = parseInt(firstClassInput.value);
+
+            const finalCost = fCount*150 + ecoCount*100;
+            var subTotals = parseInt(finalCost);
+
+            document.getElementById('sub-Total').innerText = '$'+ subTotals;
+
+            var vatCount = Math.round(subTotals*0.1);
+            document.getElementById('vat').innerText = '$'+vatCount;
 
 
-   function finalBill(){
-    const firstClassInput = document.getElementById('firstClass-count');
-    const fCount = parseInt(firstClassInput.value);
-
-    const ecoClassInput = document.getElementById('economyClass-count');
-    const ecoCount = parseInt(ecoClassInput.value);
-
-var finalCost = fCount*150 + ecoCount*100;
- var subTotalBill = parseInt(finalCost);
-document.getElementById('sub-Total').innerText = '$'+ subTotalBill;
-
-  }             
-  
-        
+            var totalBills = vatCount + subTotals;
+            document.getElementById('finalCharge').innerText =totalBills;
 
 
+        }
 
 
-
-
-
-
-
-
-
-
-
-
-
-    // document.getElementById('case-decrease').addEventListener('click',function(){
-    //     // const caseInput = document.getElementById('case-count');
-    //     // const caseCount = parseInt(caseInput.value);
-    //     // const caseNewCount = caseCount - 1;
-    //     // caseInput.value = caseNewCount
-    //     // const caseTotal = caseNewCount * 59;
-    //     // document.getElementById('case-total').innerText='$'+caseTotal;
-    //     autoMation(false);
-    
-    // });
